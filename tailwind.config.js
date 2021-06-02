2147ef49c91d37a8e5bf654165820f3fdcc8f8dc
David Ker <snappy316@gmail.com>
2021-06-01 20:15:25 -0700

    Fix TailwindCSS Typography
    - Move the `typography` object to within the `theme.extend` object
    - Change `default` key to `DEFAULT`

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,31 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         serif: ['Merriweather', ...defaultTheme.fontFamily.serif],
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            'ul > li::before': {
+              backgroundColor: defaultTheme.colors.gray[500],
+            },
+            blockquote: {
+              borderLeftColor: defaultTheme.colors.gray[500],
+            },
+            hr: {
+              borderColor: defaultTheme.colors.gray[500],
+            },
+            thead: {
+              borderBottomColor: defaultTheme.colors.gray[600],
+            },
+            'tbody tr': {
+              borderBottomColor: defaultTheme.colors.gray[500],
+            },
+          },
+        },
+      },
     },
     future: {
       removeDeprecatedGapUtilities: true,
+      purgeLayersByDefault: true,
     },
   },
   plugins: [
